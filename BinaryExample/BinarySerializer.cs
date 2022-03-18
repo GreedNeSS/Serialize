366a35bd63b1092253fcb292b014a62173d98d95
GreedNeSS <ruslan199126@gmail.com>
2022-03-18 21:54:52 +0300

    Добавьте файлы проекта.

---
+++ BinaryExample/BinarySerializer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryExample
+{
+    internal class BinarySerializer
+    {
+        public static void SaveAsBynaryFormat(object objGraph, string filename)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                try
+                {
+                    binaryFormatter.Serialize(fs, objGraph);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine("Saved obj in binary format");
+            }
+        }
+
+        public static object LoadFromBinaryFile(string filename)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (FileStream fs = File.OpenRead(filename))
+            {
+                try
+                {
+                    object obj = binaryFormatter.Deserialize(fs);
+                    Console.WriteLine("Saved obj in binary format");
+                    return obj;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ BinaryExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using BinaryExample;
+
+Console.WriteLine("***** Binary Serializer *****");
+
+Person person = new Person
+{
+    Name = "Ruslan",
+    Age = 30,
+    Id = 2314
+};
+
+string fileName = "Bynary.dat";
+
+BinarySerializer.SaveAsBynaryFormat(person, fileName);
+
+Person? loadedPerson = BinarySerializer.LoadFromBinaryFile(fileName) as Person;
+
+Console.WriteLine(loadedPerson?.Name);
